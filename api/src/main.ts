3b3dc20d912677224ba7565ae5b8f9dfe5e8b3af
Karan02 <krnptl1234@gmail.com>
2025-12-07 19:31:49 +0530

    binding port for frontend

--- api/src/main.ts
+++ api/src/main.ts
@@ -12,7 +12,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(3001);
+  await app.listen(3001,'0.0.0.0');
   console.log('API listening on http://localhost:3001');
 }
 bootstrap();
