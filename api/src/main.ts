ef3dc13d5e6bffd245a457425a5ca7ac857b096e
Karan02 <krnptl1234@gmail.com>
2025-12-08 11:59:16 +0530

    express cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -8,6 +8,7 @@ if (typeof globalThis.crypto === 'undefined') {
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
