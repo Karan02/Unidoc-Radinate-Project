0bf22b020746842cd9a6587da5fc72f8de33b4ec
Karan02 <krnptl1234@gmail.com>
2025-12-08 11:41:25 +0530

    cors enable for frontend

--- api/src/main.ts
+++ api/src/main.ts
@@ -13,7 +13,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   // await app.listen(3001,'0.0.0.0');
-  await app.getHttpServer().listen(3001, '0.0.0.0');
+  app.enableCors({
+    origin: ['http://radinate-frontend-bucket.s3-website-us-east-1.amazonaws.com/','http://localhost:3000'],
+    credentials: true
+  });
+  await app.getHttpServer().listen(3001);
   console.log('API listening on http://localhost:3001');
 }
 bootstrap();
